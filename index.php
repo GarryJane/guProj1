393f30105878c63e5d692288cf89f80ebec1eae4
GarryJane <mmm53.ripe@gmail.com>
2017-08-04 15:06:15 +0300

    some tests

--- index.php
+++ index.php
@@ -9,3 +9,11 @@ $classesDirs = [
 
 // Загрузка Классов
 ClassesLoader::load($classesDirs);
+
+
+$shopDB = new DB('127.0.0.1', 'shop', 'root', 'MoodvinAs77');
+
+var_dump( $shopDB->getAllProducts() );
+
+
+
---
+++ models/DB.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ddl
+ * Date: 04.08.17
+ * Time: 14:01
+ */
+
+class DB
+{
+
+}
