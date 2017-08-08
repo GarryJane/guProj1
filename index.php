db349eeac89813eaa52a60acf034d2e1c2ea70f0
GarryJane <mmm53.ripe@gmail.com>
2017-08-08 19:11:13 +0300

    added Class Controller

--- index.php
+++ index.php
@@ -10,15 +10,6 @@ $classesDirs = [
 // Загрузка Классов
 ClassesLoader::load($classesDirs);
 
-$shopDB = new DB('127.0.0.1', 'shop', 'root', 'MoodvinAs77');
-
-try {
-    throw new Exception("Test logging exception");
-}
-catch (Exception $e) {
-    Logger::logMsg($e->getMessage());
-}
-
-
+$page = new Controller();
 
 
--- models/Comtroller.class.php
+++ models/Comtroller.class.php
@@ -1,12 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ddl
- * Date: 08.08.17
- * Time: 18:12
- */
 
-class Comtroller
+include('Logger.class.php');
+
+class Controller
 {
+    protected $content = [];
+    protected $template = "";
+
+    public function __construct()
+    {
+        if(isset($_POST)) {
+            self::byPOST();
+        }
+
+        if(isset($_GET)) {
+             self::byGET();
+        }
+
+        self::render();
+    }
+
+    protected function byGET()
+    {
+        // Rendering by $GET['action'] value
+        switch ($_GET['page']) {
+            case 'main':
+                self::render();
+                break;
+            default:
+                self::render();
+        }
+    }
+
+    protected function byPOST()
+    {
+    }
+
+    public function render($templateName = 'main.tmpl', $varsArr = ['title' => 'MAIN PAGE'], $path = 'templates')
+    {
+        require_once 'vendor/autoload.php';
+        $loader = new Twig_Loader_Filesystem($path);
+        $twig = new Twig_Environment($loader, array('cache' => 'tmp/compilation_cache'));
+        $template = $twig->load($templateName);
+        echo $template->render($varsArr);
+    }
 
 }
